9a2f440c3e5003cc8f26a106d3a333ce726e4a3a
phamios <sonpxvn@gmail.com>
2014-05-22 10:40:30 +0700

    split master page and change something
    add more features pages

---
+++ application/views/home/widget/js_footer.php
@@ -0,0 +1,10 @@
+<div id="logged-in-component-debugjs"></div>		
+
+  
+    <div id="logged-in-component-tos_update"></div>
+    <div id="logged-in-component-meganav">		
+    </div>
+    <div id="logged-in-component-footer-components"></div>	
+    <script type="text/javascript">var DTCRequestedLoggedInComponents="account-links,debugjs,tos_update,meganav,footer-components";var DTCRequestedLoggedInComponentsArgs="Tjs=";</script>
+    <script type="text/javascript" src="<?php echo base_url('res_home/index_files'); ?>/dt-optimized-foot-js-b01f1491305a136a71d8987915470f03.js"></script>
+ 
