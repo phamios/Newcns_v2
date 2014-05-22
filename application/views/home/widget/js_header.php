a89bd892b0b5bd5fcd811211670b956ccbbf1a37
phamios <sonpxvn@gmail.com>
2014-05-22 16:34:25 +0700

    clear JS that not use

--- application/views/home/widget/js_header.php
+++ application/views/home/widget/js_header.php
@@ -1,30 +1,40 @@
 
-
 <script type="text/javascript" src="<?php echo base_url('res_home/index_files'); ?>/dt-optimized-head-js-b01f1491305a136a71d8987915470f03.js"></script>
- 
-<!--Begin Define GPT Ad Slots-->
- 
 
+<!--Begin Define GPT Ad Slots-->
+
+<script type="text/javascript">
+    
+    
+    $(document).ready(function(){
+        category_review();
+        category_news();
+        
+        $.each($("#rev_cate").find("li"), function(index, value){
+            $(value).mouseover(function(){
+                alert("ok");
+            });
+        });  
+        
+    });
+    
+    
+</script>
 <!-- DT Variables (digitaltrends-2013/functions.php) -->
 <script type="text/javascript">var ajaxurl="http://www.digitaltrends.com/wp-content/themes/digitaltrends-2013/ajax-actions.php";var ord=Math.random()*10000000000000000;</script>
 <!-- End DT Variables -->
-<script src="<?php echo base_url('res_home/index_files'); ?>/322026.js" defer="defer" async=""></script>
-<script src="<?php echo base_url('res_home/index_files'); ?>/pubads_impl_39.js" type="text/javascript" async=""></script>
-<script src="<?php echo base_url('res_home/index_files'); ?>/osd.js" type="text/javascript"></script>
-<script src="<?php echo base_url('res_home/index_files'); ?>/seg" type="text/javascript" async=""></script>
-<script src="<?php echo base_url('res_home/index_files'); ?>/nonSecureAnonymousFramework.js"></script>
+
+
+
 
 
 <script type="text/javascript">
-    $(document).ready(function(){
-       category_review();
-       category_news();
-});
+    
+     
 
-
-function category_review(){
+    function category_review(){
         $.ajax({
-            url: '<?php echo site_url('ajax/rev_cate_ajax');?>',
+            url: '<?php echo site_url('ajax/rev_cate_ajax'); ?>',
             type:'POST',
             success: function(response){ 
                 $("#rev_cate").html(response);
@@ -35,9 +45,9 @@ function category_review(){
         });
     };
     
- function category_news(){
+    function category_news(){
         $.ajax({
-            url: '<?php echo site_url('ajax/new_cate_ajax');?>',
+            url: '<?php echo site_url('ajax/new_cate_ajax'); ?>',
             type:'POST',
             success: function(response){ 
                 $("#new_cate").html(response);
