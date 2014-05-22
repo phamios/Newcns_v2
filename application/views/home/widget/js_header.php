bfbbcc4a5fa760c5db067dd1ae2a8b9659da1053
phamios <sonpxvn@gmail.com>
2014-05-22 16:56:26 +0700

    js config

--- application/views/home/widget/js_header.php
+++ application/views/home/widget/js_header.php
@@ -1,36 +1,27 @@
 
+
+
 <script type="text/javascript" src="<?php echo base_url('res_home/index_files'); ?>/dt-optimized-head-js-b01f1491305a136a71d8987915470f03.js"></script>
 
 <!--Begin Define GPT Ad Slots-->
 
+
+
+
+
 <script type="text/javascript">
     
-    
-    $(document).ready(function(){
+      $(document).ready(function(){
         category_review();
         category_news();
         
         $.each($("#rev_cate").find("li"), function(index, value){
-            $(value).mouseover(function(){
+            $(value).hover(function(){
                 alert("ok");
             });
         });  
         
     });
-    
-    
-</script>
-<!-- DT Variables (digitaltrends-2013/functions.php) -->
-<script type="text/javascript">var ajaxurl="http://www.digitaltrends.com/wp-content/themes/digitaltrends-2013/ajax-actions.php";var ord=Math.random()*10000000000000000;</script>
-<!-- End DT Variables -->
-
-
-
-
-
-<script type="text/javascript">
-    
-     
 
     function category_review(){
         $.ajax({
@@ -58,3 +49,8 @@
         });
     };
 </script>
+ 
+<!-- DT Variables (digitaltrends-2013/functions.php) -->
+<script type="text/javascript">var ajaxurl="http://www.digitaltrends.com/wp-content/themes/digitaltrends-2013/ajax-actions.php";var ord=Math.random()*10000000000000000;</script>
+<!-- End DT Variables -->
+
