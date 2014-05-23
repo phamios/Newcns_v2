5acd62ebf26b39d3e24fe416004ee93c74df7837
phamios <sonpxvn@gmail.com>
2014-05-23 22:28:50 +0700

    Revert 8879524..a089670
    This rolls back to commit 8879524220379130c2f888ffa32fd17e5883f5e8.

---
+++ application/views/home/default/default.php
@@ -0,0 +1,109 @@
+<section id="river" class="standard">
+    <h1>Recent Stories</h1>
+
+
+    <!--DTC HTML CACHE HIT START-->			
+    <?php if($listcontent <> null):?>
+    <?php foreach($listcontent as $content):?>
+    <div class="post " data-post-type="post">
+        <div class="thumb"> 
+            <a href="<?php echo site_url('home/news/'.mb_strtolower(url_title(removesign($content->post_title.'-'.$content->id))).'.html' );?>">
+                <span class="image-holder">
+                    <img src="<?php echo site_url('src/post/thumb_'.$content->post_images)?>" alt="<?php echo $content->post_title?>"/>
+                </span>							
+            </a>
+        </div>
+
+        <div class="content">
+            <h3>
+                <a href="#">
+                    <?php echo $content->post_title?>
+                </a>
+            </h3>
+
+            <p>
+                <?php echo word_limiter($content->post_description, 40); ?>
+            </p>
+
+            <div class="byline">
+                <div>
+                    <?php foreach($category as $cate):?>
+                        <?php if($cate->id == $content->cateid):?>
+                            <a class="cat" href="#">
+                                <?php echo $cate->catename;?>
+                            </a>
+                        <?php endif;?>
+                    <?php endforeach;?>
+                </div>
+
+                By <a class="author" href="#">Matt Smith</a> 
+                <span class="dash">—</span> 
+                <time datetime="<?php echo $content->post_createdate?>" pubdate="">
+                    <?php 
+                        $new_time = new DateTime($content->post_createdate);
+                        echo $new_time->format('M-d-Y');
+                    ?>
+                </time>
+            </div>
+        </div>
+    </div>
+    <?php endforeach;?>
+    <?php endif;?>
+
+
+
+    <!--DTC HTML CACHE HIT STOP-->
+
+ 
+
+
+
+    <!--DTC HTML CACHE HIT STOP-->
+
+
+    <div class="river-pagination">
+        <a href="http://www.digitaltrends.com/page/2/" class="button flat icon more"><span>See More</span></a>
+    </div>
+</section>
+
+<?php 
+
+ function removesign($str) {
+        $coDau = array("à", "á", "ạ", "ả", "ã", "â", "ầ", "ấ", "ậ", "ẩ", "ẫ", "ă", "ằ", "ắ"
+            , "ặ", "ẳ", "ẵ", "è", "é", "ẹ", "ẻ", "ẽ", "ê", "ề", "ế", "ệ", "ể", "ễ", "ì", "í", "ị", "ỉ", "ĩ",
+            "ò", "ó", "ọ", "ỏ", "õ", "ô", "ồ", "ố", "ộ", "ổ", "ỗ", "ơ"
+            , "ờ", "ớ", "ợ", "ở", "ỡ",
+            "ù", "ú", "ụ", "ủ", "ũ", "ư", "ừ", "ứ", "ự", "ử", "ữ",
+            "ỳ", "ý", "ỵ", "ỷ", "ỹ",
+            "đ",
+            "À", "Á", "Ạ", "Ả", "Ã", "Â", "Ầ", "Ấ", "Ậ", "Ẩ", "Ẫ", "Ă"
+            , "Ằ", "Ắ", "Ặ", "Ẳ", "Ẵ",
+            "È", "É", "Ẹ", "Ẻ", "Ẽ", "Ê", "Ề", "Ế", "Ệ", "Ể", "Ễ",
+            "Ì", "Í", "Ị", "Ỉ", "Ĩ",
+            "Ò", "Ó", "Ọ", "Ỏ", "Õ", "Ô", "Ồ", "Ố", "Ộ", "Ổ", "Ỗ", "Ơ"
+            , "Ờ", "Ớ", "Ợ", "Ở", "Ỡ",
+            "Ù", "Ú", "Ụ", "Ủ", "Ũ", "Ư", "Ừ", "Ứ", "Ự", "Ử", "Ữ",
+            "Ỳ", "Ý", "Ỵ", "Ỷ", "Ỹ",
+            "Đ", "ê", "ù", "à");
+        $khongDau = array("a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a"
+            , "a", "a", "a", "a", "a", "a",
+            "e", "e", "e", "e", "e", "e", "e", "e", "e", "e", "e",
+            "i", "i", "i", "i", "i",
+            "o", "o", "o", "o", "o", "o", "o", "o", "o", "o", "o", "o"
+            , "o", "o", "o", "o", "o",
+            "u", "u", "u", "u", "u", "u", "u", "u", "u", "u", "u",
+            "y", "y", "y", "y", "y",
+            "d",
+            "A", "A", "A", "A", "A", "A", "A", "A", "A", "A", "A", "A"
+            , "A", "A", "A", "A", "A",
+            "E", "E", "E", "E", "E", "E", "E", "E", "E", "E", "E",
+            "I", "I", "I", "I", "I",
+            "O", "O", "O", "O", "O", "O", "O", "O", "O", "O", "O", "O"
+            , "O", "O", "O", "O", "O",
+            "U", "U", "U", "U", "U", "U", "U", "U", "U", "U", "U",
+            "Y", "Y", "Y", "Y", "Y",
+            "D", "e", "u", "a");
+        return str_replace($coDau, $khongDau, $str);
+    }
+    
+?>
---
+++ application/views/home/widget/navbar.php
@@ -0,0 +1,80 @@
+<div id="nav-bar">
+    <div class="bb-wrap">
+        <nav class="primary mega">
+            <!--DTC HTML CACHE HIT START-->			
+            <ul id="menu-primary-navigation-2013" class="menu">
+                <li id="menu-item-411326" class="sub menu-item menu-item-type-post_type menu-item-object-page menu-item-has-children menu-item-411326">
+                    <a href="http://www.digitaltrends.com/product-reviews/">Product Reviews</a>
+                    <div class="mega-wrap">
+                        <div id="rev_cate"> 
+                        </div> 
+                    </div>
+                </li>
+                <li id="menu-item-411314" class="sub menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children menu-item-411314"><a>News</a>
+                    <div class="mega-wrap">
+                        <div id="new_cate"> 
+                        </div> 
+                    </div>
+                </li>
+
+                <li id="menu-item-411327" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-411327">
+                    <a href="http://www.digitaltrends.com/videos/">Videos</a>
+                </li>
+
+                <li id="menu-item-520473" class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-520473 menu-item-object-id-19">
+                    <a href="http://www.digitaltrends.com/features/">Features</a>
+                </li>
+
+                <li id="menu-item-552394" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-552394">
+                    <a href="http://deals.digitaltrends.com/">Deals</a>
+                </li>
+            </ul>
+            <!--DTC HTML CACHE HIT STOP-->
+        </nav>
+
+
+        <!--DTC HTML CACHE HIT START-->			
+        <div id="sitewide-social">
+
+            <div class="facebook cached">
+                <div class="wrap">
+                    <div class="expand" data-app-class="fb-like">
+                        <div class="app-class" data-href="http://www.facebook.com/digitaltrendsftw" data-layout="button_count" data-send="false" data-width="100" data-show-faces="false"></div>
+                    </div>
+                    <span class="icon"></span>
+                    <span class="num">246<sup>K</sup></span>
+                </div>
+            </div>
+
+            <div class="twitter cached">
+                <div class="wrap">
+                    <div class="expand" data-app-class="twitter-follow-button">
+                        <a class="app-class" data-show-count="false" data-width="60px" href="https://twitter.com/DigitalTrends"></a>
+                    </div>
+                    <span class="icon"></span>
+                    <span class="num">99<sup>K</sup></span>
+                </div>
+            </div>
+
+            <div class="gplus cached">
+                <div class="wrap">
+                    <div class="expand" data-app-class="g-plusone">
+                        <div>
+                            <div class="app-class" data-href="https://plus.google.com/105021553983255532005/posts" data-size="medium" data-callback="gotoGplusPage" data-onstartinteraction="dtGplusStartInteraction" data-onendinteraction="dtGplusEndInteraction"></div>
+                        </div>
+                    </div>
+                    <span class="icon"></span>
+                    <span class="num">26<sup>K</sup></span>
+                </div>
+            </div>
+
+            <a class="rss" href="http://www.digitaltrends.com/feed/" title="Subscribe to our RSS Feed">
+                <div class="wrap">
+                    <span class="icon"></span>
+                </div>
+            </a>
+        </div>
+
+        <!--DTC HTML CACHE HIT STOP-->
+    </div>
+</div>
