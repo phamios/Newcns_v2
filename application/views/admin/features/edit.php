a16ea299e9dee7cf3c21d25125c708ddf89fc2e8
phamios <sonpxvn@gmail.com>
2014-05-26 23:35:33 +0700

    change TinyMCE with more Plugin

--- application/views/admin/features/edit.php
+++ application/views/admin/features/edit.php
@@ -1,30 +1,35 @@
 <script src="http://tinymce.cachefly.net/4.0/tinymce.min.js"></script>
 <script type="text/javascript">
 tinymce.init({
-    selector: "textarea",
-    plugins : 'advlist autolink link image lists charmap print preview',
-    toolbar: "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image",
-    font_formats: "Andale Mono=andale mono,times;"+
-        "Arial=arial,helvetica,sans-serif;"+
-        "Arial Black=arial black,avant garde;"+
-        "Book Antiqua=book antiqua,palatino;"+
-        "Comic Sans MS=comic sans ms,sans-serif;"+
-        "Courier New=courier new,courier;"+
-        "Georgia=georgia,palatino;"+
-        "Helvetica=helvetica;"+
-        "Impact=impact,chicago;"+
-        "Symbol=symbol;"+
-        "Tahoma=tahoma,arial,helvetica,sans-serif;"+
-        "Terminal=terminal,monaco;"+
-        "Times New Roman=times new roman,times;"+
-        "Trebuchet MS=trebuchet ms,geneva;"+
-        "Verdana=verdana,geneva;"+
-        "Webdings=webdings;"+
-        "Wingdings=wingdings,zapf dingbats"
+        selector: "textarea",
+        plugins: [
+                "advlist autolink autosave link image lists charmap print preview hr anchor pagebreak spellchecker",
+                "searchreplace wordcount visualblocks visualchars code fullscreen insertdatetime media nonbreaking",
+                "table contextmenu directionality emoticons template textcolor paste fullpage textcolor"
+        ],
 
-    
-});
-</script>
+        toolbar1: "newdocument fullpage | bold italic underline strikethrough | alignleft aligncenter alignright alignjustify | styleselect formatselect fontselect fontsizeselect",
+        toolbar2: "cut copy paste | searchreplace | bullist numlist | outdent indent blockquote | undo redo | link unlink anchor image media code | inserttime preview | forecolor backcolor",
+        toolbar3: "table | hr removeformat | subscript superscript | charmap emoticons | print fullscreen | ltr rtl | spellchecker | visualchars visualblocks nonbreaking template pagebreak restoredraft",
+
+        menubar: false,
+        toolbar_items_size: 'small',
+
+        style_formats: [
+                {title: 'Bold text', inline: 'b'},
+                {title: 'Red text', inline: 'span', styles: {color: '#ff0000'}},
+                {title: 'Red header', block: 'h1', styles: {color: '#ff0000'}},
+                {title: 'Example 1', inline: 'span', classes: 'example1'},
+                {title: 'Example 2', inline: 'span', classes: 'example2'},
+                {title: 'Table styles'},
+                {title: 'Table row 1', selector: 'tr', classes: 'tablerow1'}
+        ],
+
+        templates: [
+                {title: 'Test template 1', content: 'Test 1'},
+                {title: 'Test template 2', content: 'Test 2'}
+        ]
+});</script>
 <div class="row-fluid">
     <div class="span12">
         <div class="box dark">
