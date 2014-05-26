ce57a62331b1e0195e4c5b43c7b7d53c615320e9
phamios <sonpxvn@gmail.com>
2014-05-26 15:41:39 +0700

    change header

--- application/views/home/widget/header.php
+++ application/views/home/widget/header.php
@@ -26,23 +26,34 @@
         <meta property="article:published_time" content="2013-05-20T20:23:50+00:00">
         <meta property="article:modified_time" content="2014-02-05T10:52:02+00:00">
         <meta property="og:image" content="<?php echo base_url('res_home/newcnlogo.png');?>">
-        <meta name="twitter:card" content="summary">
-        <meta name="twitter:site" content="@digitaltrends">
-        <meta name="twitter:domain" content="Digital Trends">
-        <meta name="twitter:creator" content="@digitaltrends">
-        <meta name="twitter:image" content=""><!-- / Yoast WordPress SEO plugin. -->
-        
- 
-    
+  
         <link rel="stylesheet" id="dt-css-b39b787d60a3d8e785cd09b859cd2848-css" href="<?php echo base_url('res_home/index_files'); ?>/dt-optimized-css-b39b787d60a3d8e785cd09b859cd2848.css" type="text/css" media="all">
-
-        <link rel="EditURI" type="application/rsd+xml" title="RSD" href="http://www.digitaltrends.com/xmlrpc.php?rsd">
+ 
         <link rel="shortlink" href="<?php echo site_url();?>?p=444226">
 
 
        <?php $this->load->view('home/widget/js_header');?>
 
-        <style type="text/css" id="tyntShCSS">#tyntSh div,#tyntSh span,#tyntSh img,#tyntSh a,div#tyntSh{margin:0;padding:0;border:0;font-size:100%;font:inherit;vertical-align:baseline;line-height:1;background:0;text-align:left}#tyntSh img,#tyntSh span{display:inline}#tyntSh div,div#tyntSh{display:block}div#tyntSh{z-index:9999999;position:absolute;height:62px;margin:10px;overflow:visible;background:#FFF;border-radius:5px;text-align:center;box-shadow: 0 2px 6px rgba(0,0,0,0.5)}div#tyntShTr{margin:-11px auto 0 auto;width:0;height:0;border-left:10px solid transparent;border-right:10px solid transparent;border-bottom:11px solid #F1F1F1}img#tyntShTw,img#tyntShP,img#tyntShFb,img#tyntShLi,img#tyntShGp{cursor:pointer;height:28px;width:28px;margin:3px 3px 2px 3px;vertical-align:bottom}div#tyntShPTL,div#tyntShHFL{color:#888;text-decoration:none;margin:0 0 2px 0;background-color:#F1F1F1;padding:2px 4px;border-radius:5px 5px 0 0}div#tyntShHFL{text-align:right;border-radius:0 0 5px 5px;margin:2px 0 0 0}div#tyntShHFL,span#tyntShHFLs,div#tyntShPTL{font:normal 8px Arial}span#tyntShHFLs{cursor:pointer;}span#tyntShHFLs:hover{color:#555;text-decoration:underline}div#tyntShCB{position:absolute;cursor:pointer;width:14px;height:14px;top:0;right:0;background-position:0 -14px;background-repeat:no-repeat}div#tyntShCB:hover{background-position: 0 0}div.tyntShIh,.tyntShIh div,.tyntShIh span{margin:0;padding:0;border:0;font-size:100%;font:inherit;vertical-align:baseline;line-height:1;background:0;text-align:left}.tyntShIh img,.tyntShIh span{display:inline}.tyntShIh div,div.tyntShIh{display:block;outline:0}div.tyntShIh{position:absolute;cursor:default;background:transparent url(http://cdn.tynt.com/images/ss/pi.png) no-repeat;width:43px;height:21px;opacity:0;filter:alpha(opacity=0);z-index:9999999;-webkit-transition:opacity 0.5s;-moz-transition:opacity 0.5s}div.tyntIhHover{opacity:1;filter:alpha(opacity=100);cursor:pointer}</style>
+        <style type="text/css" id="tyntShCSS">#tyntSh div,#tyntSh span,#tyntSh img,#tyntSh a,div#tyntSh
+            {margin:0;padding:0;border:0;font-size:100%;font:inherit;vertical-align:baseline;line-height:1;background:0;text-align:left}
+            #tyntSh img,#tyntSh span{display:inline}
+            #tyntSh div,div#tyntSh{display:block}
+            div#tyntSh{z-index:9999999;position:absolute;height:62px;margin:10px;overflow:visible;background:#FFF;border-radius:5px;text-align:center;box-shadow: 0 2px 6px rgba(0,0,0,0.5)}
+            div#tyntShTr{margin:-11px auto 0 auto;width:0;height:0;border-left:10px solid transparent;border-right:10px solid transparent;border-bottom:11px solid #F1F1F1}
+            img#tyntShTw,img#tyntShP,img#tyntShFb,img#tyntShLi,img#tyntShGp{cursor:pointer;height:28px;width:28px;margin:3px 3px 2px 3px;vertical-align:bottom}
+            div#tyntShPTL,div#tyntShHFL{color:#888;text-decoration:none;margin:0 0 2px 0;background-color:#F1F1F1;padding:2px 4px;border-radius:5px 5px 0 0}
+            div#tyntShHFL{text-align:right;border-radius:0 0 5px 5px;margin:2px 0 0 0}
+            div#tyntShHFL,span#tyntShHFLs,div#tyntShPTL{font:normal 8px Arial}
+            span#tyntShHFLs{cursor:pointer;}
+            span#tyntShHFLs:hover{color:#555;text-decoration:underline}
+            div#tyntShCB{position:absolute;cursor:pointer;width:14px;height:14px;top:0;right:0;background-position:0 -14px;background-repeat:no-repeat}
+            div#tyntShCB:hover{background-position: 0 0}
+            div.tyntShIh,.tyntShIh div,.tyntShIh span{margin:0;padding:0;border:0;font-size:100%;font:inherit;vertical-align:baseline;line-height:1;background:0;text-align:left}
+            .tyntShIh img,.tyntShIh span{display:inline}
+            .tyntShIh div,div.tyntShIh{display:block;outline:0}
+            div.tyntShIh{position:absolute;cursor:default;background:transparent url(http://cdn.tynt.com/images/ss/pi.png) no-repeat;width:43px;height:21px;opacity:0;filter:alpha(opacity=0);z-index:9999999;-webkit-transition:opacity 0.5s;-moz-transition:opacity 0.5s}
+            div.tyntIhHover{opacity:1;filter:alpha(opacity=100);cursor:pointer}
+        </style>
 
      
 
